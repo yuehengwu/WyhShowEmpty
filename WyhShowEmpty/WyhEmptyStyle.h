e90c5b0f3b7411ceee0d6bd411e517f1acf1bd98
wyh <yueheng.wu@net263.com>
2018-06-22 15:10:01 +0800

    Fix bug that WyhEmptyStyle superView property cause retain cycle  .

--- WyhShowEmpty/WyhEmptyStyle.h
+++ WyhShowEmpty/WyhEmptyStyle.h
@@ -159,7 +159,7 @@ typedef NS_ENUM(NSInteger, imageType)
 /**
  控件的父视图，主要是为了用户自定义superView 默认为控制器的view
  */
-@property (nonatomic, strong) UIView *superView;
+@property (nonatomic, weak) UIView *superView;
 
 
 
